fe2d5abf42f3d5aa76e7b55c615a1b4fa8c0508c
MEGA <MEGA@gmail.com>
2025-02-17 12:19:16 +0200

    add n

--- GitHub-Test/Program.cs
+++ GitHub-Test/Program.cs
@@ -2,8 +2,11 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            int n = 1;
+            Console.WriteLine($"{n++}promqna");
             A a = new A();
             a.Print();
         }
