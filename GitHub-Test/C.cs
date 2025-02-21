7d31ff975b75212c7809d8421ec195af6b0c9484
MEGA <MEGA@gmail.com>
2025-02-21 11:18:59 +0200

    add c

---
+++ GitHub-Test/C.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Test
+{
+    internal class C
+    {
+        public double Sqrt(double a)
+        {
+            return Math.Sqrt(a);
+        }
+    }
+}
